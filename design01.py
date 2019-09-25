b4de4ed5703ab65c9d9146a870e937118c00d83c
Tina <tinataleb@berkeley.edu>
2019-09-25 20:33:01 +0100

    adding the code from pi

---
+++ design01.py
@@ -0,0 +1,105 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+import picamera     #camera library
+import pygame as pg #audio library
+import os           #communicate with os/command line
+
+from google.cloud import vision  #gcp vision library
+from time import sleep
+from adafruit_crickit import crickit
+#import time
+import signal
+import sys
+import re           #regular expression lib for string searches!
+
+#set up your GCP credentials - replace the " " in the following line with your .json file and path
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="../cred.json"
+
+# this line connects to Google Cloud Vision! 
+client = vision.ImageAnnotatorClient()
+
+# global variable for our image file - to be captured soon!
+image = 'image.jpg'
+
+
+def stream(camera):
+    
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    # capture frames from the camera
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        image = frame.array
+
+        # show the frame
+        cv2.imshow("Frame", image)
+        key = cv2.waitKey(1) & 0xFF
+
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+        
+        output_capture(camera)
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+def takephoto(camera):
+    
+    # this triggers an on-screen preview, so you know what you're photographing!
+    camera.start_preview() 
+#    sleep(.5)                   #give it a pause so you can adjust if needed
+    camera.capture('image.jpg') #save the image
+    camera.stop_preview()       #stop the preview
+    
+def web_search(image):
+    #this function sends your image to google cloud using the
+    #web_detection method, collects a response, and parses that
+    #response for the 'best web association' found for the image.
+    #there's no actuation here - just printing - but you can easily
+    #engage with speaker_out() or motor_turn() if you like!
+    
+    response = client.web_detection(image=image)
+    web_guess = response.web_detection
+    
+    for label in web_guess.best_guess_labels:
+        print('Best Web Guess Label: {}'.format(label.label))
+
+def output_capture(camera):
+    takephoto(camera) # First take a picture
+    """Run a label request on a single image"""
+
+    with open('image.jpg', 'rb') as image_file:
+        #read the image file
+        content = image_file.read()
+        #convert the image file to a GCP Vision-friendly type
+        image = vision.types.Image(content=content)
+
+        web_search(image)
+#        time.sleep(0.1)
+    
+def main():
+    
+    #generate a camera object for the takephoto function to
+    #work with
+#    camera = picamera.PiCamera()
+
+    # initialize the camera and grab a reference to the raw camera capture
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+    
+
+    # allow the camera to warmup
+    time.sleep(0.1)
+    
+    #setup our pygame mixer to play audio in subsequent stages
+    
+    stream(camera)
+          
+        
+if __name__ == '__main__':
+        main() 
